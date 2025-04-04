add5175b8ba05906d372b859a33d1027f7d527ce
Xanadar0 <61867465+Xanadar0@users.noreply.github.com>
2025-04-04 17:34:28 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ const output = document.querySelector('#name-output');
 
 input.addEventListener("input", (event) => {
     if (input.value.trim() === "") {   
-        output.textContent = "Anonymous!";
+        output.textContent = "Anonymous";
     } else {
         output.textContent = input.value.trim();
     }
